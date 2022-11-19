a93f2f049630c457469da1f55eac92de06e07fcf
AC Tech <118635023+vb-at-actech@users.noreply.github.com>
2022-11-19 23:04:24 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+  </header>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,76 @@
+<script>
+//import { RouterLink, RouterView } from "vue-router";
+import NavBar from "@/components/NavigationBar.vue";
+export default {
+  components: { NavBar },
+};
+</script>
+
+<template>
+  <!-- <main>
+    <TheWelcome />
+  </main> -->
+
+  <!-- <button
+    class="btn btn-primary"
+    data-bs-target="#collapseTarget"
+    data-bs-toggle="collapse"
+  >
+    Bootstrap collapse
+  </button>
+  <div class="collapse py-2" id="collapseTarget">
+    This is the toggle-able content!
+  </div> -->
+
+  <NavBar />
+
+  <div class="mb-3">
+    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="mb-3">
+    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <button 
+    class="btn btn-primary" 
+    data-bs-target="#collapseTarget" 
+    data-bs-toggle="collapse">
+    Bootstrap collapse
+  </button>
+  <div class="collapse py-2" id="collapseTarget">
+    This is the toggle-able content!
+  </div>
+</template>
